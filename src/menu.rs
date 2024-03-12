58e852267681e68dabb6f6cb84e05ea9255ebdf1
Wojciech Gawronski <wgawronski@white-rook.pl>
2024-03-12 18:12:40 +0000

    Day 12: Small fix - again, to preserve streak.

--- src/menu.rs
+++ src/menu.rs
@@ -200,7 +200,7 @@ fn main_menu_setup(mut commands: Commands) {
                         ))
                         .with_children(|parent| {
                             parent.spawn(TextBundle::from_section(
-                                "New Game",
+                                "Start Game",
                                 button_text_style.clone(),
                             ));
                         });

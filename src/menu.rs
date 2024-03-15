2a4734c9865e0372cf221a84d988ba8f3ef362ed
Wojciech Gawronski <wgawronski@white-rook.pl>
2024-03-15 21:08:10 +0000

    Day 15: Small fix - again, to preserve streak.

--- src/menu.rs
+++ src/menu.rs
@@ -200,7 +200,7 @@ fn main_menu_setup(mut commands: Commands) {
                         ))
                         .with_children(|parent| {
                             parent.spawn(TextBundle::from_section(
-                                "Start Game",
+                                "New Game",
                                 button_text_style.clone(),
                             ));
                         });
@@ -215,7 +215,7 @@ fn main_menu_setup(mut commands: Commands) {
                         ))
                         .with_children(|parent| {
                             parent.spawn(TextBundle::from_section(
-                                "Configuration",
+                                "Settings",
                                 button_text_style.clone(),
                             ));
                         });
